f416cc29aa55b48817d12b4dc87cfac5a0bc3ad5
Chethan Reddy <95994938+Chethu28@users.noreply.github.com>
2023-11-20 23:41:36 +0530

    docker

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull chethan28/simple-python-flask-app:latest
+docker pull chethanreddy28/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app:latest
+docker run -d -p 5000:5000 chethareddy28/simple-python-flask-app:latest
