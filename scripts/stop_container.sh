fc8e7f41b98a10366c8d60fd2fd407d6c35b02de
Chethan Reddy <95994938+Chethu28@users.noreply.github.com>
2023-11-20 23:10:28 +0530

    docker stocontainer chnages

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+!/bin/bash
 set -e
 
 # Stop the running container (if any)
 containerid=`sudo docker ps | awk -F " " '{print $1}`
-docker stop containerid
+docker stop $containerid
 docker rm -f $containerid
