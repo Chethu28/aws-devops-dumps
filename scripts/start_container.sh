371db41a0d274e9e2ff70dcf0ccb92d06796a5be
Chethan Reddy <95994938+Chethu28@users.noreply.github.com>
2023-11-21 11:36:18 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull chethanreddy28/simple-python-flask-app
+docker pull docker pull chethanreddy28/aws-flast-python-new-app:latest
 # Run the Docker image as a container
-docker run -d -p 5000:5000 chethareddy28/simple-python-flask-app
+docker run -d -p 5000:5000 chethanreddy28/aws-flast-python-new-app:latest
